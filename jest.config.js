1634f82a5a219db62379a6d8c50fea2de28a6181
duckduckcody <cody.hennighan@gmail.com>
2022-06-05 17:04:45 +0800

    add coverage options to test run

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['configs/**/*.ts', 'utils/**/*.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules'],
 };
