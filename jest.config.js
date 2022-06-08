ae1783be333ebce801a084e6d726739b6984d262
duckduckcody <cody.hennighan@gmail.com>
2022-06-08 13:25:26 +0800

    add scripts to jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['configs/**/*.ts', 'utils/**/*.ts'],
+  collectCoverageFrom: ['configs/**/*.ts', 'utils/**/*.ts', 'scripts/**/*.ts'],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules'],
 };
