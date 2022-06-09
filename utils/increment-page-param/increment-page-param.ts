9bcf3100a2d81985160c8dd2c7f4890ed9cfb25b
duckduckcody <cody.hennighan@gmail.com>
2022-06-09 14:11:35 +0800

    comment increment page param

--- utils/increment-page-param/increment-page-param.ts
+++ utils/increment-page-param/increment-page-param.ts
@@ -1,3 +1,9 @@
+/**
+ * for a url increment its page param by 1 or set to 1 if no page value present
+ * @param url
+ * @param pageParamKey optional name of param key. Defaults to 'page'
+ * @returns url with incremented page param value
+ */
 export const incrementPageParam = (
   url: string,
   pageParamKey: string = 'page'
