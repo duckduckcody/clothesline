4e9beaefd3da8304e5d3da3576c72f596754b332
duckduckcody <cody.hennighan@gmail.com>
2022-10-23 12:15:07 +0800

    add all configs

--- configs/crawler-configs.ts
+++ configs/crawler-configs.ts
@@ -1,9 +1,12 @@
 import { CrawlerConfig } from '../types/CrawlerConfig';
+import { coolShirtzConfig } from './cool-shirtz-config';
+import { cultureKingsConfig } from './culture-kings-config';
+import { edgeClothingConfig } from './edge-clothing/edge-clothing-config';
 import { universalConfig } from './universal/universal-config';
 
 export const crawlerConfigs: CrawlerConfig[] = [
-  // coolShirtzConfig,
-  // cultureKingsConfig,
-  // edgeClothingConfig,
+  coolShirtzConfig,
+  cultureKingsConfig,
+  edgeClothingConfig,
   universalConfig,
 ];
